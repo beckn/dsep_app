332329451fc7d622a556fa8cebe6780820a5acbf
Ujjwal Tiwari <ujjwaltiwari@Ujjwals-MacBook-Pro.local>
2023-08-17 08:56:34 +0530

    initial project push

---
+++ hooks/useLanguage.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router'
+import en from '../locales/en'
+import fa from '../locales/fa'
+
+export const useLanguage = () => {
+    const { locale } = useRouter()
+    const t = locale === 'en' ? en : fa
+
+    return { t, locale }
+}
